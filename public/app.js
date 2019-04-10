03fd682a81bbe5d0927745550f91e7a5c925083a
Jeffrey Gilbert <jegilbert@conversantmedia.com>
2019-04-09 21:31:16 -0500

    Rendering stats in the top left corner are a nice touch

--- public/app.js
+++ public/app.js
@@ -133,9 +133,16 @@ const render = () => {
 };
 
 
+var stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
 const animate = () => {
-	requestAnimationFrame(animate);
+	stats.begin();
 	render();
+	stats.end();
+
+	requestAnimationFrame(animate);
 };
 
 
