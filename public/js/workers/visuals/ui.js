52f830f52f86cd81177d205e107700e2b88cd012
Ager, Kathryn <kager@conversantmedia.com>
2019-04-22 16:27:37 -0500

    text displays in center of adContainer

--- public/js/workers/visuals/ui.js
+++ public/js/workers/visuals/ui.js
@@ -33,6 +33,11 @@ function UserInterface(THREE, canvas) {
 		height: 0
 	};
 
+	const adContainerDims = {
+		width: 8,
+		height:4
+	}
+
 	// current state of rotation animation
 	let angle = 0;
 
@@ -68,18 +73,18 @@ function UserInterface(THREE, canvas) {
 		const visibleHeightAtZDepth = (depth, camera) => {
 			// compensate for cameras not positioned at z=0
 			const cameraOffset = camera.position.z;
-			if ( depth < cameraOffset ) depth -= cameraOffset;
+			if (depth < cameraOffset) depth -= cameraOffset;
 			else depth += cameraOffset;
-		
+
 			// vertical fov in radians
-			const vFOV = camera.fov * Math.PI / 180; 
-		
+			const vFOV = camera.fov * Math.PI / 180;
+
 			// Math.abs to ensure the result is always positive
-			return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+			return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
 		};
-		
-		const visibleWidthAtZDepth = ( depth, camera ) => {
-			const height = visibleHeightAtZDepth( depth, camera );
+
+		const visibleWidthAtZDepth = (depth, camera) => {
+			const height = visibleHeightAtZDepth(depth, camera);
 			return height * camera.aspect;
 		};
 
@@ -97,17 +102,17 @@ function UserInterface(THREE, canvas) {
 			const aspect = appWidth / appHeight;
 			scene = new THREE.Scene();
 
-	//		scene.matrixAutoUpdate = false;
-			
+			//		scene.matrixAutoUpdate = false;
+
 			const fogColor = new THREE.Color(0x000000);
 			scene.background = fogColor;
-			scene.fog = new THREE.Fog(fogColor, 20, 65);	
+			scene.fog = new THREE.Fog(fogColor, 20, 65);
 
 			// PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
 			camera = new THREE.PerspectiveCamera(50, aspect, 0.01, 200); // 40
 			visibleBounds = getVisibleBounds(50, camera);
 
-	//		camera.matrixAutoUpdate = false;
+			//		camera.matrixAutoUpdate = false;
 
 			// WebGL 2 looks to be supported in Chrome and FF, but not in Safari Tech Preview very well.
 			context = canvas.getContext('webgl2'); // webgl2 for that engine
@@ -117,25 +122,24 @@ function UserInterface(THREE, canvas) {
 				context: context,
 				antialias: true
 			});
-			
+
 			renderer.setSize(appWidth, appHeight);
 
 			// positioning a light above the camera
 			light = new THREE.PointLight(0xFFFFFF, 1);
 			light.position.set(200, 250, 600);
 			light.castShadow = true;
-			light.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 50, .5, 200, 2000 ) );
+			light.shadow = new THREE.LightShadow(new THREE.PerspectiveCamera(50, .5, 200, 2000));
 			light.shadow.bias = - 0.000222;
 			light.shadow.mapSize.width = 1024;
 			light.shadow.mapSize.height = 1024;
 			scene.add(light);
-
 			// scene.updateMatrix();
 
-			renderer.setSize( appWidth, appHeight );
+			renderer.setSize(appWidth, appHeight);
 			camera.aspect = appWidth / appHeight;
 			camera.updateProjectionMatrix();
-		
+
 			pageCenterDims.width = window.innerWidth / 2;
 			pageCenterDims.height = appHeight / 2;
 
@@ -149,27 +153,27 @@ function UserInterface(THREE, canvas) {
 			console.log('after createPoints', Date.now() - startTime);
 			return loadFonts().then(font => {
 				console.log('after loadFonts', Date.now() - startTime);
-				const midway = config.maxWidth/2 + config.xPosition;
+				const midway = config.maxWidth / 2 + config.xPosition;
 				console.log('Total objects in universe:', points.length);
 				return generateMaterials(font).then(letterShapeGeoms => {
 					console.log('after generateMaterials', Date.now() - startTime);
 					return generateGeometries(letterShapeGeoms, points, midway);
 				});
 			});
-		}).then().then(geometries => {
+		}).then(geometries => {
 			console.log('after generateGeometries', Date.now() - startTime);
 			return addLettersToScene(geometries);
-		}).then(()=>{
+		}).then(() => {
 			console.log('after addLettersToScene', Date.now() - startTime);
 			return new Promise(resolve => {
 
 				// Ad container is required by render function
-				// drawAdContainer(video);
+				drawAdContainer();
 
 				render();
 				resolve();
 			});
-		}).then(()=>{
+		}).then(() => {
 			console.log('after first render', Date.now() - startTime);
 			animating = true;
 			requestAnimationFrame(animate);
@@ -178,13 +182,12 @@ function UserInterface(THREE, canvas) {
 
 
 	var textBlock;
-	var adContainer = new THREE.Group(); 
-
+	var adContainer = new THREE.Group();
 	// this is where all the profile layout stuff needs to happen
-	const drawAdContainer = () => {
-		
-		const bgMesh = new THREE.Mesh( 
-			new THREE.PlaneBufferGeometry( 8, 4, 1, 1 ), 
+	const drawAdContainer = async () => {
+
+		const bgMesh = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(adContainerDims.width, adContainerDims.height, 1, 1),
 			new THREE.MeshPhysicalMaterial({ // this should probably be basic
 				color: new THREE.Color(0x2277FF),
 				transparent: true,
@@ -198,8 +201,8 @@ function UserInterface(THREE, canvas) {
 			})
 		);
 
-		const shadowMesh = new THREE.Mesh( 
-			new THREE.PlaneBufferGeometry( 5, 4, 1, 1 ), 
+		const shadowMesh = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(5, 4, 1, 1),
 			new THREE.ShadowMaterial({ // this should probably be basic
 				opacity: 0.9,
 				color: new THREE.Color(0x000000),
@@ -208,14 +211,36 @@ function UserInterface(THREE, canvas) {
 		shadowMesh.position.z = 0.2; // depth
 		shadowMesh.receiveShadow = true;
 
+
 		adContainer.add(shadowMesh);
 		adContainer.add(bgMesh);
-
-		scene.add( adContainer );
+		let mesh = await drawLetters();
+		adContainer.add(mesh)
+		scene.add(adContainer);
 	};
 
+	const drawLetters = async () => {
+
+		return loadFonts().then(font => {
+			var textGeo = new THREE.TextBufferGeometry("This is some text", {
+				font: font,
+				size: 0.1,
+				height: 1,
+				curveSegments: 12,
+				bevelEnabled: false,
+			});
+			textGeo.computeBoundingBox();
+			var centerOffset = - 0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+			var textMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000, specular: 0xffffff });
+			var mesh = new THREE.Mesh(textGeo, textMaterial);
+			mesh.position.x = centerOffset;
+			return mesh;
+		})
+	}
+
+
 	let cameraTweens = [];
-	let cameraTarget = {x:0,y:0,z:0};
+	let cameraTarget = { x: 0, y: 0, z: 0 };
 
 	const rateOfTravel = {
 		x: 500 / 1000,
@@ -223,7 +248,7 @@ function UserInterface(THREE, canvas) {
 		z: Math.PI / 1000
 	}
 
-	this.updatePerspective = (x,y,z) => {
+	this.updatePerspective = (x, y, z) => {
 		if (!animating) { return; }
 		cameraTarget.x = x;
 		cameraTarget.y = y;
@@ -240,25 +265,30 @@ function UserInterface(THREE, canvas) {
 			cameraTweens.pop().kill();
 			cameraTweens.pop().kill();
 		}
-	
+
 		cameraTweens.push(TweenMax.to(camera.position, xDuration, {
 			x: cameraTarget.x,
-			ease: Power1.easeOut, 
-			onComplete: () => {console.log('finished tween', camera.position.x)}
+			ease: Power1.easeOut,
+			onComplete: () => {
+				console.log('finished tween', camera.position.x)
+			}
 		}));
 
 		cameraTweens.push(TweenMax.to(camera.rotation, yDuration, {
 			y: cameraTarget.y,
-			ease: Power1.easeOut, 
-			onComplete: () => {console.log('finished tween', camera.rotation.y)}
+			ease: Power1.easeOut,
+			onComplete: () => {
+				console.log('finished tween', camera.rotation.y)
+			}
 		}));
 	};
 
+
 	// This is stuff that could happen at any interval driven by any animation loop (tweenmax for instance, or just a timeout, or raf)
 	const render = () => {
-		
+
 		angle = (angle + 0.1) % 360;
-		
+
 		// Tilted axis of the camera to the scene
 		// camera.position.x = cameraOriginDims.x + (boxRotationDims.x * -1); // @face now
 		camera.rotation.x = rads(angle + 270);
@@ -269,14 +299,14 @@ function UserInterface(THREE, canvas) {
 		camera.position.z = cameraHoverDistance * Math.sin(rads(angle));
 		// camera.updateMatrix();
 
-		/* temporarily disabled while attempting to get the app running in workers
+		// /* temporarily disabled while attempting to get the app running in workers
 		adContainer.rotation.x = camera.rotation.x;
 		adContainer.rotation.y = (cameraOriginDims.x + (boxRotationDims.x * -1)) * -0.001;
 
 		adContainer.position.x = camera.position.x * 1.03;
 		adContainer.position.y = camera.position.y * 0.9;
 		adContainer.position.z = camera.position.z * 0.9;
-		*/
+		// */
 
 		light.rotation.x = camera.rotation.x;
 		light.rotation.y = (cameraOriginDims.x + (boxRotationDims.x * -1)) * 0.02;
@@ -295,7 +325,7 @@ function UserInterface(THREE, canvas) {
 	const animate = () => {
 		if (!animating) { return; }
 		render();
-		requestAnimationFrame(()=>{
+		requestAnimationFrame(() => {
 			animate();
 		});
 	};
@@ -303,7 +333,7 @@ function UserInterface(THREE, canvas) {
 	const posRandRate = 0.9;
 
 	const generatePlantedForest = (xPosition, maxWidth, numWidthIncrement, maxRadius, minRadius, numRadiusIncrements) => {
-		
+
 		let points = [];
 		const twoPi = 2 * Math.PI;
 		let circumference,
@@ -316,7 +346,7 @@ function UserInterface(THREE, canvas) {
 			endingAngle = twoPi * maxArcLength / circumference;
 			return posRandRate * Math.random() * (endingAngle - startingAngle) + startingAngle;
 		}
-		
+
 		let angle;
 
 		const generateRandomPointOnArc = (currentRadius, minArcLength, maxArcLength, arcRadius) => {
@@ -332,19 +362,19 @@ function UserInterface(THREE, canvas) {
 
 		const generatePoint = (leftWidthBoundary, widthOffset, currentMinArc, currentMaxArc, minRadius, maxRadius, widthVariance) => {
 			radius = minRadius + (maxRadius - minRadius) * Math.random() * posRandRate;
-			
+
 			pointOnArc = generateRandomPointOnArc(radius, currentMinArc, currentMaxArc, minRadius);
 
 			return new Promise((resolve, reject) => {
-				points.push({ 
-					x: ((widthVariance ? widthOffset * posRandRate * Math.random() : 0)+ leftWidthBoundary),
+				points.push({
+					x: ((widthVariance ? widthOffset * posRandRate * Math.random() : 0) + leftWidthBoundary),
 					y: pointOnArc.y,
 					z: pointOnArc.z
 				});
 				resolve(); // point
 			});
 		}
-		
+
 		let numMinArcSteps,
 			minArcIncrement,
 			currentMinArc,
@@ -352,42 +382,42 @@ function UserInterface(THREE, canvas) {
 			currentArcIncrement;
 
 		const generateRing = (leftWidthBoundary, widthOffset, minRadius, maxRadius, widthVariance) => {
-			return new Promise(resolve=>{
+			return new Promise(resolve => {
 				numMinArcSteps = Math.ceil((minRadius * twoPi) / (widthOffset));
 				minArcIncrement = (minRadius * twoPi) / numMinArcSteps;
-				
+
 				let promises = [];
 
 				for (currentArcIncrement = 0; currentArcIncrement < numMinArcSteps; currentArcIncrement++) {
 					currentMinArc = currentArcIncrement * minArcIncrement;
 					currentMaxArc = currentMinArc + minArcIncrement;
-					
+
 					promises.push(generatePoint(leftWidthBoundary, widthOffset, currentMinArc, currentMaxArc, minRadius, maxRadius, widthVariance));
 				}
-	
-				Promise.all(promises).then(()=>{ // verticies
+
+				Promise.all(promises).then(() => { // verticies
 					resolve(); // vertices
 				});
 			})
 		}
-		
+
 		let radiusIncrement,
 			currentRadius;
 
 		const generateDisk = (leftWidthBoundary, widthOffset, maxRadius, minRadius, numRadiusIncrements, widthVariance) => {
 			return new Promise(resolve => {
-				radiusIncrement = (maxRadius - minRadius)/ numRadiusIncrements;
-	
+				radiusIncrement = (maxRadius - minRadius) / numRadiusIncrements;
+
 				let promises = [];
 				//Generate points on inner diameter
 				promises.push(generateRing(leftWidthBoundary, widthOffset, minRadius, minRadius, widthVariance));
 				// Generate points inside ring
-				for(currentRadius = minRadius; currentRadius < maxRadius; currentRadius += radiusIncrement) {
+				for (currentRadius = minRadius; currentRadius < maxRadius; currentRadius += radiusIncrement) {
 					promises.push(generateRing(leftWidthBoundary, widthOffset, currentRadius, currentRadius + radiusIncrement, widthVariance));
 				}
 				//Generate points on outer diameter
 				promises.push(generateRing(leftWidthBoundary, widthOffset, maxRadius, maxRadius, widthVariance));
-				
+
 				Promise.all(promises).then(() => { // pointArrays
 					// let vertices = [];
 					// pointArrays.forEach(ring=>{
@@ -402,15 +432,15 @@ function UserInterface(THREE, canvas) {
 		return new Promise(resolve => {
 			const widthIncrement = maxWidth / numWidthIncrement;
 			const endWith = xPosition + maxWidth;
-			
+
 			let startTime = Date.now();
 			let firstSet = generateDisk(xPosition, widthIncrement, maxRadius, minRadius, numRadiusIncrements, false);
 
 			let promises = [];
-			for(let currentXPosition = xPosition; currentXPosition < endWith; currentXPosition += widthIncrement) {
+			for (let currentXPosition = xPosition; currentXPosition < endWith; currentXPosition += widthIncrement) {
 				promises = [...promises, generateDisk(currentXPosition, widthIncrement, maxRadius, minRadius, numRadiusIncrements, true)];
 			}
-			
+
 			let lastSet = generateDisk(endWith, widthIncrement, maxRadius, minRadius, numRadiusIncrements, false);
 			console.log('after last promise chain', Date.now() - startTime);
 
@@ -452,7 +482,7 @@ function UserInterface(THREE, canvas) {
 		const config = { // this is the dev config because it starts up faster, so your refreshes arent slow
 			maxWidth: 90,
 			xPosition: (90 / 2) * -1, // maxwidth / 2 * -1
-			numWidthIncrement: 30,
+			numWidthIncrement: 10,
 			maxRadius: 45,
 			minRadius: 20,
 			numRadiusIncrements: 5
@@ -460,14 +490,14 @@ function UserInterface(THREE, canvas) {
 
 		return new Promise(resolve => {
 			generatePlantedForest(config.xPosition,
-				config.maxWidth, 
-				config.numWidthIncrement, 
-				config.maxRadius, 
-				config.minRadius, 
+				config.maxWidth,
+				config.numWidthIncrement,
+				config.maxRadius,
+				config.minRadius,
 				config.numRadiusIncrements).then((points) => {
-				// loadFonts
-				resolve({config,points});
-			});
+					// loadFonts
+					resolve({ config, points });
+				});
 		});
 	};
 
@@ -492,7 +522,7 @@ function UserInterface(THREE, canvas) {
 				geometry.computeBoundingBox();
 				geometry.translate(-0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x), 0, 0);
 				letterShapeGeoms[index] = geometry;
-			}).then(()=>{
+			}).then(() => {
 				resolve(letterShapeGeoms);
 			});
 		});
@@ -503,12 +533,12 @@ function UserInterface(THREE, canvas) {
 			rotY,
 			letterPos,
 			newGeom;
-	
-		const halfPi = Math.PI/2;
+
+		const halfPi = Math.PI / 2;
 
 		return new Promise(resolve => {
-			spaceWorkOut(points, (pos)=>{
-				if (pos.x < midway - 15){
+			spaceWorkOut(points, (pos) => {
+				if (pos.x < midway - 15) {
 					rotY = 2;
 				} else if (pos.x > midway + 15) {
 					rotY = 0;
@@ -525,12 +555,12 @@ function UserInterface(THREE, canvas) {
 				newGeom = letterShapeGeoms[letterPos].clone();
 
 				newGeom.rotateX(Math.atan2(pos.z, pos.y) + 3 * halfPi);
-		//		newGeom.rotateY(rotY);
+				//		newGeom.rotateY(rotY);
 				// plane.rotation.y = plane.rotation.y + rotY;
-				newGeom.translate( pos.x, pos.y, pos.z );
+				newGeom.translate(pos.x, pos.y, pos.z);
 
 				geometries[rotY][letterPos].push(newGeom);
-			}).then(()=>{
+			}).then(() => {
 				resolve(geometries);
 			});
 		});
@@ -550,24 +580,24 @@ function UserInterface(THREE, canvas) {
 
 		console.log('start add letters to scene', Date.now() - startTime);
 		return new Promise(resolve => {
-			console.log('kickoff', geometries);
-			spaceWorkOut(geometries, (letterGroups, rotIndex)=>{
-				console.log('loop:',rotIndex);
+			// console.log('kickoff', geometries);
+			spaceWorkOut(geometries, (letterGroups, rotIndex) => {
+				// console.log('loop:',rotIndex);
 				spaceWorkOut(letterGroups, (geoms, letterIndex) => {
-					console.log('loop:', rotIndex, letterIndex)
-					mesh = new THREE.Mesh( THREE.BufferGeometryUtils.mergeBufferGeometries(geoms, true), colors[rotIndex]);
+					// console.log('loop:', rotIndex, letterIndex)
+					mesh = new THREE.Mesh(THREE.BufferGeometryUtils.mergeBufferGeometries(geoms, true), colors[rotIndex]);
 					mesh.updateMatrix();
 					mesh.matrixAutoUpdate = false;
 					// mesh.rotation.x = mesh.rotation.x;
 					// mesh.rotation.y = mesh.rotation.y;
 					meshes.push(mesh);
-				}).then(()=>{
-					spaceWorkOut(meshes, (mesh)=>{
+				}).then(() => {
+					spaceWorkOut(meshes, (mesh) => {
 						console.log('add mesh to scene:', mesh, Date.now() - startTime);
 						scene.add(mesh);
 					});
 				});
-			}).then(()=>{
+			}).then(() => {
 				console.log('done adding letter meshes to scene:', Date.now() - startTime);
 				resolve();
 			});
